5c37ad718b08fa925f344007301c020f56a1fbbc
Yuki <choppin.block.party@gmail.com>
2018-10-13 03:56:32 +0200

    Run emacs through zsh

--- config.h
+++ config.h
@@ -61,7 +61,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]  = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *emacscmd[]  = { "emacs", NULL };
+// Start Emacs from zsh because environment are important for it
+static const char *emacscmd[]  = { "/bin/zsh", "-c", "emacs", NULL };
 static const char *firefoxcmd[]= { "firefox", NULL };
 static const char *termcmd[]   = { "urxvt", NULL };
 
