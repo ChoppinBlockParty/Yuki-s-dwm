b0d5ae11de233ba7a3917a66e0212e7e58884d92
Yuki <choppin.block.party@gmail.com>
2020-06-21 21:00:56 +0100

    Remove clang setup in install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,6 @@ set -e
 
 sudo apt-get install -y --no-install-recommends xorg libx11-dev libxft-dev fonts-dejavu fonts-freefont-ttf lightdm slick-greeter network-manager-gnome gnome-keyring parcellite unclutter kbdd xsel libxinerama-dev
 
-if [ -x "$(command -v clang 2>/dev/null)" ]; then
-  export CC=clang
-  export CXX=clang++
-  export AR=llvm-ar
-  export RANLIB=llvm-ranlib
-fi
-
 make clean || true
 make
 sudo make install
